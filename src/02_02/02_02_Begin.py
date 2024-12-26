265d2758730732b5ec4fd23d5005758e055937e9
DigiFab AI <digifabai@gmail.com>
2024-12-26 03:25:21 +0000

    changes made to 02_02 and 02_03

--- src/02_02/02_02_Begin.py
+++ src/02_02/02_02_Begin.py
@@ -7,6 +7,8 @@ from sklearn.preprocessing import LabelEncoder, StandardScaler
 # Load the dataset
 data = pd.read_csv("input/insurance.csv")
 
+# Fill missing values in the 'bmi' column with the mean
+data['bmi'] = data['bmi'].fillna(data['bmi'].mean())
 # Display the first 15 rows of the dataset
 print("First 15 rows of the dataset:")
 print(data.head(15))
@@ -23,20 +25,24 @@ X_final = pd.concat([X_num, region_df, data['sex'], data['smoker']], axis=1)  # 
 y_final = data['charges']  # Target variable
 
 # Split the data into train and test sets
-X_train, X_test, y_train, y_test = train_test_split(X_final, y_final, test_size=0.33, random_state=0)
+X_train, X_test, y_train, y_test = train_test_split(
+    X_final, y_final, test_size=0.33, random_state=0
+)
 
 # Standardize the features
 s_scaler = StandardScaler()
-X_train = s_scaler.fit_transform(X_train.astype(np.float_))
-X_test = s_scaler.transform(X_test.astype(np.float_))
+X_train = s_scaler.fit_transform(X_train.astype(np.float64))
+X_test = s_scaler.transform(X_test.astype(np.float64))
+
 
 # TODO: Fit the Linear Regression model on the training data
+
 # TODO: Predict on both training and test datasets
-# TODO: Print the coefficients, intercept, and R-squared scores
-# TODO: Print model performance scores
-#print score
+
+# Print the coefficients, intercept, and R-squared scores
 print("lr.coef_: {}".format(lr.coef_))
 print("lr.intercept_: {}".format(lr.intercept_))
-print('lr train score %.3f, lr test score: %.3f' % (
-lr.score(X_train,y_train),
-lr.score(X_test, y_test)))
+print(
+    "lr train score %.3f, lr test score: %.3f"
+    % (lr.score(X_train, y_train), lr.score(X_test, y_test))
+)
